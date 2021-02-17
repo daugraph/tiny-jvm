b299eefbe04b445913914e1d7278d9bd0971f75d
daugraph <daugraph@qq.com>
2021-02-17 20:16:16 +0800

    add constant integer info

---
+++ ch03/classfile/cp_numeric.go
@@ -0,0 +1,12 @@
+package classfile
+
+import "math"
+
+type ConstantIntegerInfo struct {
+  val int32
+}
+
+func (self *ConstantIntegerInfo) readInfo(reader *ClassReader) {
+  bytes := reader.readUint32()
+  self.val = int32(bytes)
+}
